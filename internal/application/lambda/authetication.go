4600548ac896a9f33f36567d328a1c881d511881
Matthew Casperson <matthewcasperson@gmail.com>
2023-09-20 13:28:04 +1000

    Ignore case

--- internal/application/lambda/authetication.go
+++ internal/application/lambda/authetication.go
@@ -4,11 +4,19 @@ import (
 	"errors"
 	"github.com/OctopusSolutionsEngineering/OctopusSubscriptionListener/internal/domain/handlers"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/samber/lo"
+	"strings"
 )
 
 func Authenticate(request events.APIGatewayV2HTTPRequest) error {
-	if apiKey, ok := request.Headers["x-octopussubscriptionlistener-apikey"]; ok {
-		err := handlers.IsAuthenticated(apiKey)
+	apiKey := lo.PickBy(request.Headers, func(key string, value string) bool {
+		return strings.ToLower(key) == "x-octopussubscriptionlistener-apikey"
+	})
+
+	entries := lo.Entries(apiKey)
+
+	if len(entries) == 1 {
+		err := handlers.IsAuthenticated(entries[0].Value)
 		if err != nil {
 			return err
 		}
