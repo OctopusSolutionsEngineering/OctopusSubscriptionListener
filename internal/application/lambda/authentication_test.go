b6f76de96644330419078cab7ce7ee8ee741dca4
Matthew Casperson <matthewcasperson@gmail.com>
2023-09-20 19:20:05 +1000

    Added more tests

--- internal/application/lambda/authentication_test.go
+++ internal/application/lambda/authentication_test.go
@@ -18,3 +18,29 @@ func TestHeaderParsing(t *testing.T) {
 		t.Fatalf("RAuthenication should not have failed")
 	}
 }
+
+func TestHeaderParsingFail(t *testing.T) {
+	os.Setenv("APIKEY", "MyKey1")
+	err := Authenticate(events.APIGatewayV2HTTPRequest{
+		Headers: map[string]string{
+			"X-OctopusSubscriptionListener-ApiKey": "MyKey",
+		},
+	})
+
+	if err == nil {
+		t.Fatalf("RAuthenication should have failed")
+	}
+}
+
+func TestHeaderParsingFailEmpty(t *testing.T) {
+	os.Setenv("APIKEY", "")
+	err := Authenticate(events.APIGatewayV2HTTPRequest{
+		Headers: map[string]string{
+			"X-OctopusSubscriptionListener-ApiKey": "MyKey",
+		},
+	})
+
+	if err == nil {
+		t.Fatalf("RAuthenication should have failed")
+	}
+}
